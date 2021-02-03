ea3f23010a2e8d701da4907f9e887ef8d6ef7a49
zongronghuang <zongrong.h886@gmail.com>
2021-02-03 17:32:04 +0800

    feat: add react router to App

--- src/App.js
+++ src/App.js
@@ -4,14 +4,32 @@ import React from "react";
 import TodoApp from './views/TodoApp.js'
 import Login from './views/Login.js'
 import { useState, useEffect } from "react";
+import { Switch, Route, Redirect, useRouteMatch } from 'react-router-dom'
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState('Login')
+  const isLogin = false
+  const isAtLogin = useRouteMatch('/login')
+
+  if (!isLogin && !isAtLogin) {
+    return <Redirect to="/login" />
+  }
 
   return (
     <div className="app">
-      {currentPage === 'Login' && <Login />}
-      {currentPage === 'TodoApp' && <TodoApp />}
+      <Switch>
+        <Route exact path="/">
+          {isLogin ? <Redirect to="/todos" /> : <Redirect to="/login" />}
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/todos">
+          <TodoApp />
+        </Route>
+      </Switch>
     </div>
   )
 }
